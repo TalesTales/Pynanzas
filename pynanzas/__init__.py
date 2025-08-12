45c6f15b5f6ff1f7267e312fe738ee6e8ea18de8
TalesTales <servicios@pipebonilla.com>
2025-08-12 17:50:12 -0500

    style(init): se cambiaron las constantes mOVIMIENTOS_* por MOVS_*

--- pynanzas/__init__.py
+++ pynanzas/__init__.py
@@ -6,29 +6,29 @@ Pynanzas: Un paquete para el análisis y seguimiento de portafolios de inversió
 from .analisis import dist_riesgo
 from .constants import (
     BD_SQLITE,
-    MOVIMIENTOS_APORTES,
-    MOVIMIENTOS_INTERESES,
-    MOVIMIENTOS_NO_APORTANTES,
+    MOVS_APORTES,
+    MOVS_INTERESES,
+    MOVS_NO_APORTES,
     PROD_ID,
 )
 from .data_loader import cargar_datos
 from .limpiar_datos import prods_raw_a_df, trans_raw_to_df
 from .portafolio import Portafolio
 from .producto import ProductoFinanciero
-from .sql import crear_tabla_prods, crear_tabla_trans
+from .sql import crear_tabla_movs, crear_tabla_prods
 
 __all__ = [
     "Portafolio",
     "ProductoFinanciero",
-    "MOVIMIENTOS_APORTES",
-    "MOVIMIENTOS_INTERESES",
-    "MOVIMIENTOS_NO_APORTANTES",
+    "MOVS_APORTES",
+    "MOVS_INTERESES",
+    "MOVS_NO_APORTES",
     "PROD_ID",
     "PROD_ID",
     "dist_riesgo",
     "BD_SQLITE",
     'crear_tabla_prods',
-    'crear_tabla_trans',
+    'crear_tabla_movs',
     'cargar_datos',
     'prods_raw_a_df',
     'trans_raw_to_df'
