6f9caac0fe733a43beb68562c466dd6ae9e31462
TalesTales <servicios@pipebonilla.com>
2025-08-12 14:42:07 -0500

    fix(init): se movió la lógica de cargar datos al notebook

--- pynanzas/__init__.py
+++ pynanzas/__init__.py
@@ -2,7 +2,6 @@
 Pynanzas: Un paquete para el análisis y seguimiento de portafolios de inversión.
 """
 
-import pandas as pd
 
 from .analisis import dist_riesgo
 from .constants import (
@@ -10,6 +9,7 @@ from .constants import (
     MOVIMIENTOS_APORTES,
     MOVIMIENTOS_INTERESES,
     MOVIMIENTOS_NO_APORTANTES,
+    PROD_ID,
 )
 from .data_loader import cargar_datos
 from .limpiar_datos import prods_raw_a_df, trans_raw_to_df
@@ -24,20 +24,14 @@ __all__ = [
     "MOVIMIENTOS_INTERESES",
     "MOVIMIENTOS_NO_APORTANTES",
     "PROD_ID",
-    "DF_PRODS",
-    "DF_TRANS",
     "PROD_ID",
     "dist_riesgo",
     "BD_SQLITE",
     'crear_tabla_prods',
-    'crear_tabla_trans'
+    'crear_tabla_trans',
+    'cargar_datos',
+    'prods_raw_a_df',
+    'trans_raw_to_df'
 ]
 
-DATOS: dict[str, pd.DataFrame] = cargar_datos()
-DF_PRODS: pd.DataFrame = prods_raw_a_df(
-    DATOS["productos"], DATOS["diccionario"]
-)
-DF_TRANS: pd.DataFrame = trans_raw_to_df(DATOS["transacciones"])
-PROD_ID: str = DATOS["diccionario"]["Ficha"].iloc[0].lower().replace(" ", "_")
 
-print("__init__:Datos cargados")
