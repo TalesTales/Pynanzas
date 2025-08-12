a06a23c161f3323ae6e8c73f220d9eae40b4c719
TalesTales <servicios@pipebonilla.com>
2025-08-12 17:53:59 -0500

    fix(sql): Se ajustaron todos los parametros para que usen las constantes correctas

--- pynanzas/sql.py
+++ pynanzas/sql.py
@@ -1,10 +1,10 @@
 import sqlite3
 from typing import Optional
 
-from .constants import BD_SQLITE
+from .constants import BD_SQLITE, PROD_ID, TABLA_MOVS, TABLA_PRODS
 
 
-def crear_tabla_prods(nombre_tabla: str = 'productos',
+def crear_tabla_prods(nombre_tabla: str = TABLA_PRODS,
                       nombre_columnas: Optional[dict[str,str]] = None,
                       nombre_bd: str = BD_SQLITE) -> None:
     """Crea una tabla de productos financieros en una base de datos SQLite.
@@ -105,62 +105,65 @@ def crear_tabla_prods(nombre_tabla: str = 'productos',
     for k, v in nombre_columnas.items():
         orden_ddl += f'{k} {v}'
         if k != ultima_k:
-            orden_ddl += ',\n'
+            orden_ddl += ',\n' #TODO: usar .join
 
     try:
         with sqlite3.connect(nombre_bd) as conn:
             cursor: sqlite3.Cursor = conn.cursor()
-            ddl_completo: str = f"""CREATE TABLE IF NOT EXISTS {nombre_tabla} (\n{orden_ddl}\n);"""
+            ddl_completo: str = f"""CREATE TABLE IF NOT EXISTS {nombre_tabla}
+            (\n{orden_ddl}\n);"""
             print(ddl_completo) #TODO: logging
             cursor.execute(ddl_completo)
             conn.commit()
     except sqlite3.Error as e:
         print(f"sql.crear_tabla_prods: error sql {e}")
 
-def crear_tabla_trans(nombre_tabla: str = 'transacciones',
-                      nombre_columnas: Optional[dict[str, str]] = None,
-                      nombre_tabla_prods: str = 'productos',
-                      nombre_bd: str = BD_SQLITE) -> None:
+def crear_tabla_movs(nombre_tabla: str = TABLA_MOVS,
+                     nombre_columnas: Optional[dict[str, str]] = None,
+                     nombre_tabla_prods: str = TABLA_PRODS,
+                     producto_id: str = PROD_ID,
+                     nombre_bd: str = BD_SQLITE) -> None:
     if nombre_bd == '':
-        raise ValueError('crear_tabla_trans: nombre_bd vacio')
+        raise ValueError('crear_tabla_movs: nombre_bd vacio')
     if nombre_tabla == '':
-        raise ValueError('crear_tabla_trans: nombre_tabla vacio')
+        raise ValueError('crear_tabla_movs: nombre_tabla vacio')
     if nombre_tabla_prods == '':
-        raise ValueError('crear_tabla_trans: nombre_tabla_prods vacio')
+        raise ValueError('crear_tabla_movs: nombre_tabla_prods vacio')
     if nombre_columnas is None or len(nombre_columnas) == 0:
         nombre_columnas = {
             "id":            "INTEGER PRIMARY KEY AUTOINCREMENT",
-            "producto_id":   "TEXT NOT NULL",
+            producto_id:   "TEXT NOT NULL",
             "fecha":         "DATE NOT NULL",
             "tipo":          "TEXT NOT NULL",
             "valor":         "REAL NOT NULL",
             "unidades":      "REAL",
             "valor_uniadad": "REAL",
         }
-    ultima_k: str = next(reversed(nombre_columnas.keys()))
     orden_ddl: str = ''
     for k, v in nombre_columnas.items():
         orden_ddl += f'{k} {v}'
         orden_ddl += ',\n'
-    orden_ddl = orden_ddl + f"FOREIGN KEY (producto_id) REFERENCES {nombre_tabla_prods} (producto_id)"
+    orden_ddl = orden_ddl + f"""FOREIGN KEY ({producto_id}) REFERENCES
+    {nombre_tabla_prods} ({producto_id})"""
     try:
         with sqlite3.connect(nombre_bd) as conn:
             cursor: sqlite3.Cursor = conn.cursor()
-            if not _tabla_prods_existe(cursor, nombre_bd, nombre_tabla_prods):
+            if not _tabla_prods_existe(cursor, nombre_tabla_prods):
                 crear_tabla_prods(nombre_tabla=nombre_tabla_prods,
                                   nombre_bd=nombre_bd)
-            ddl_completo: str = f"CREATE TABLE IF NOT EXISTS {nombre_tabla} (\n{orden_ddl}\n);"
+            ddl_completo: str = f"""CREATE TABLE IF NOT EXISTS {nombre_tabla}
+            (\n{orden_ddl}\n);"""
             print(ddl_completo)  #TODO: logging
             cursor.execute(ddl_completo)
             conn.commit()
     except sqlite3.Error as e:
-        print(f"sql.crear_tabla_trans: error sql {e}")
+        print(f"sql.crear_tabla_movs: error sql {e}")
 
 
 def _tabla_prods_existe(cursor,
-                        nombre_bd: str = BD_SQLITE,
-                        nombre_tabla_prods: str = 'productos',
+                        nombre_tabla_prods: str = TABLA_PRODS,
                         ) -> bool:
-    query_tabla_prods: str = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{nombre_tabla_prods}'"
+    query_tabla_prods: str = f"""SELECT name FROM sqlite_master WHERE
+    type='table' AND name='{nombre_tabla_prods}'"""
     cursor.execute(query_tabla_prods)
     return bool(cursor.fetchall())
