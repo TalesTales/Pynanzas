51660ecc2787065c534388573a2458a742e32883
TalesTales <servicios@pipebonilla.com>
2025-08-15 15:14:30 -0500

    fix(init): agregado dicionario a la importacion global del modulo

--- pynanzas/__init__.py
+++ pynanzas/__init__.py
@@ -2,28 +2,34 @@
 Pynanzas: Un paquete para el análisis y seguimiento de portafolios de inversión.
 """
 
-
 from .analisis import dist_riesgo
 from .constants import (
     BD_SQLITE,
-    MOVS_APORTES,
-    MOVS_INTERESES,
-    MOVS_NO_APORTES,
     PROD_ID,
 )
 from .data_loader import cargar_datos
+from .diccionario import (
+    MovsAportes,
+    MovsIntereses,
+    MovsNoAportes,
+    NomBD,
+    NomTablas,
+    Riesgo,
+)
 from .limpiar_datos import prods_raw_a_df, trans_raw_to_df
 from .portafolio import Portafolio
 from .producto import ProductoFinanciero
-from .sql import crear_tabla_movs, crear_tabla_prods
+from .sql import (
+    EsquemaMovs,
+    EsquemaProds,
+    crear_tabla_movs,
+    crear_tabla_prods,
+    insertar_prod,
+)
 
 __all__ = [
     "Portafolio",
     "ProductoFinanciero",
-    "MOVS_APORTES",
-    "MOVS_INTERESES",
-    "MOVS_NO_APORTES",
-    "PROD_ID",
     "PROD_ID",
     "dist_riesgo",
     "BD_SQLITE",
@@ -31,7 +37,16 @@ __all__ = [
     'crear_tabla_movs',
     'cargar_datos',
     'prods_raw_a_df',
-    'trans_raw_to_df'
+    'trans_raw_to_df',
+    'EsquemaMovs',
+    'EsquemaProds',
+    'insertar_prod',
+    'NomTablas',
+    'NomBD',
+    'Riesgo',
+    'MovsAportes',
+    'MovsNoAportes',
+    'MovsIntereses',
 ]
 
 
