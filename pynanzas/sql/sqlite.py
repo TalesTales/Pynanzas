8ba691daee266c09b9774f76f86be55929eaa0d8
TalesTales <servicios@pipebonilla.com>
2025-08-19 18:23:30 -0500

    style(sqlite): se ajustó para que tenga la lógica de tabla_existe

--- pynanzas/sql/sqlite.py
+++ pynanzas/sql/sqlite.py
@@ -3,36 +3,35 @@ from typing import Any, Optional
 
 from pynanzas.constants import PROD_ID
 from pynanzas.sql.diccionario import NomBD, NomTablas
-from pynanzas.sql.movs import EsquemaMovs, crear_tabla_movs
-from pynanzas.sql.prods import (
-    EsquemaProds,
-    crear_tabla_prods,
-    tabla_prods_existe,
-)
+from pynanzas.sql.esquemas import EsquemaMovs, EsquemaProds
 
 
 def actualizar_tabla(nom_tabla: NomTablas,
                      esquema: EsquemaProds | EsquemaMovs,
                      nom_bd: NomBD = NomBD.BD_SQLITE,
                      cursor: Optional[sqlite3.Cursor] = None
-                     )->None:
+                     ) -> None:
+    # Importaciones locales para evitar ciclos
+    from pynanzas.sql.movs import crear_tabla_movs
+    from pynanzas.sql.prods import crear_tabla_prods
+
     with (sqlite3.connect(nom_bd) as con):
         if cursor is None:
             cursor = con.cursor()
-        cursor.execute ("SELECT name FROM sqlite_master WHERE "
-                             "type='table' AND name=?", (nom_tabla,))
+        cursor.execute("SELECT name FROM sqlite_master WHERE "
+                       "type='table' AND name=?", (nom_tabla,))
         if not cursor.fetchall():
             if isinstance(esquema, EsquemaProds):
                 crear_tabla_prods(esquema, nom_tabla, nom_bd)
-            else:
-                if not tabla_prods_existe(cursor, nom_tabla):
+            elif isinstance(esquema, EsquemaMovs):
+                if not tabla_existe(cursor, NomTablas.PRODS):
                     crear_tabla_prods(nom_bd=nom_bd)
                 crear_tabla_movs(esquema, nom_tabla, NomTablas.PRODS,
                                  PROD_ID, nom_bd)
             return
         cursor.execute(f"PRAGMA table_info ({nom_tabla})")
         resultado: list[Any] = cursor.fetchall()
-        set_colums_pragma: set[str] =set([n[1] for n in resultado])
+        set_colums_pragma: set[str] = set([n[1] for n in resultado])
         if set_colums_pragma == set(esquema.keys()):
             return
         else:
@@ -50,3 +49,13 @@ def actualizar_tabla(nom_tabla: NomTablas,
                         f"ALTER TABLE {nom_tabla} DROP COLUMN {colum}")
                 con.commit()
             return
+
+
+def tabla_existe(
+        cursor: sqlite3.Cursor,
+        nom_tabla: NomTablas,
+) -> bool:
+    query: str = ("SELECT name FROM sqlite_master WHERE type='table' AND "
+                  "name=?")
+    cursor.execute(query, (nom_tabla,))
+    return bool(cursor.fetchall())
