f7b4d48c028cd266eab1d94087c2aaaaabee2a55
TalesTales <servicios@pipebonilla.com>
2025-08-19 18:21:08 -0500

    style(SQL/Init): agregadas nuevas funciones al all

--- pynanzas/sql/__init__.py
+++ pynanzas/sql/__init__.py
@@ -1,5 +1,8 @@
-from pynanzas.sql.movs import EsquemaMovs, crear_tabla_movs, insertar_mov
-from pynanzas.sql.prods import EsquemaProds, crear_tabla_prods, insertar_prod
+
+from pynanzas.sql.diccionario import NomBD, NomTablas
+from pynanzas.sql.esquemas import EsquemaMovs, EsquemaProds
+from pynanzas.sql.movs import crear_tabla_movs, insertar_mov
+from pynanzas.sql.prods import crear_tabla_prods, insertar_prod
 from pynanzas.sql.sqlite import actualizar_tabla
 
 __all__ = [
@@ -9,5 +12,7 @@ __all__ = [
     'insertar_mov',
     'crear_tabla_prods',
     'insertar_prod',
-    'actualizar_tabla'
+    'actualizar_tabla',
+    'NomTablas',
+    'NomBD'
 ]
