679941b53dbda1b4b78b25649b1cfe77ec332c3f
TalesTales <servicios@pipebonilla.com>
2025-08-26 16:50:16 -0500

    feat(limpiar_data): Se re-implementó prods_csv_a_df, movs_csv_a_df, prods_sql_a_df y usando LazyFrame y DataFrame desde polars

--- pynanzas/limpiar_data.py
+++ pynanzas/limpiar_data.py
@@ -1,35 +1,46 @@
-#%%
-from dataclasses import asdict
+from pathlib import Path
 
-import pandas as pd
+import polars as pl
 
-from pynanzas.cargar_data import cargar_csv_a_df
-from pynanzas.constants import PROD_ID
-from pynanzas.sql.diccionario import NomTablas
-from pynanzas.sql.esquemas import EsquemaMovs, EsquemaProds
+from pynanzas.cargar_data import cargar_csv_a_df, cargar_sql_a_df
+from pynanzas.constants import DIR_DATA
+from pynanzas.sql.diccionario import NomTablas, PATH_DB, PathDB
 
-ESQUEMA_PRODS_DF = EsquemaProds(
-    'string','string','string','boolean','string','category','category',
-    'category','string', 'string','string', 'string','string','boolean',
-    'float32','float32','float32','float32','float32','datetime64[s]')
 
 def prods_csv_a_df(nom_tabla: NomTablas = NomTablas.PRODS,
-                   esquema: EsquemaProds = ESQUEMA_PRODS_DF) -> pd.DataFrame:
-    df_prods_raw: pd.DataFrame = cargar_csv_a_df(nom_tabla)
-    df_prods_raw.drop_duplicates(inplace=True)
-    df_prods = df_prods_raw.astype(asdict(esquema))
-    df_prods.dropna(inplace=True)
-    df_prods.set_index(PROD_ID, inplace=True)
-    return df_prods
-
-ESQUEMA_MOVS_DF = EsquemaMovs(
-    'string','datetime64[s]','string','float64','float64','float64',
-    'int64','datetime64[ns]')
+                   dir_data: Path = DIR_DATA) -> pl.DataFrame:
+    csv_prods_raw: pl.LazyFrame = (cargar_csv_a_df(nom_tabla,dir_data)
+                                  .with_columns(
+        (pl.col("fecha_actualizacion").str.to_datetime(time_unit="ms")))
+                                  .cast({"simulado": pl.Boolean,
+                                         "riesgo": pl.UInt8,
+                                         "liquidez": pl.UInt8,
+                                         "plazo": pl.UInt8,
+                                         "abierto": pl.Boolean})
+                                  )
+    return csv_prods_raw.collect()
 
 def movs_csv_a_df(nom_tabla: NomTablas = NomTablas.MOVS,
-                  esquema: EsquemaMovs = ESQUEMA_MOVS_DF) -> pd.DataFrame:
-    df_movs_raw: pd.DataFrame = cargar_csv_a_df(nom_tabla)
-    df_movs = df_movs_raw.astype(asdict(esquema))
-    df_movs.drop_duplicates(inplace=True)
-    df_movs.set_index('id', inplace=True, drop=True)
-    return df_movs
+                  dir_data: Path = DIR_DATA) -> pl.DataFrame:
+    csv_movs_raw = (cargar_csv_a_df(nom_tabla, dir_data)
+                   .with_columns((pl.col("fecha").str.to_datetime()),
+                                 (pl.col("fecha_agregada").str.to_datetime())
+                                 ))
+    return csv_movs_raw.collect()
+
+def prods_sql_a_df(nom_tabla: NomTablas = NomTablas.PRODS,
+                   nom_bd: PathDB = PATH_DB) -> pl.DataFrame:
+    sql_prods_raw: pl.LazyFrame = (cargar_sql_a_df(nom_tabla,nom_bd).lazy()
+                                  .with_columns(
+        (pl.col("fecha_actualizacion").str.to_datetime(time_unit="ms")))
+                                  .cast({"simulado": pl.Boolean,
+                                         "riesgo": pl.UInt8,
+                                         "liquidez": pl.UInt8,
+                                         "plazo": pl.UInt8,
+                                         "abierto": pl.Boolean})
+                                  )
+    return sql_prods_raw.collect()
+
+if __name__ == '__main__':
+    print( prods_sql_a_df().head())
+    pass
