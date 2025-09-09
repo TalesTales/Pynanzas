2edb02b1aa3fda86b4f580916f2a73285c5e6580
Felipe Bonilla <fa.bonilla@Mac-mini-de-PS-2.local>
2025-09-09 17:45:36 -0500

    refactor(limpiar_data): se actualizó el uso de la función cargar_data

--- pynanzas/limpiar_data.py
+++ pynanzas/limpiar_data.py
@@ -2,9 +2,9 @@ from pathlib import Path
 
 import polars as pl
 
-from pynanzas.cargar_data import cargar_csv_a_df, cargar_sql_a_df
+from pynanzas.cargar_data import cargar_csv_a_df, cargar_ddb_a_lf
 from pynanzas.constants import DIR_DATA
-from pynanzas.sql.diccionario import URI, NomTablas
+from pynanzas.sql.diccionario import NomTablas
 
 
 def prods_csv_a_lf(nom_tabla: NomTablas = NomTablas.PRODS,
@@ -27,22 +27,20 @@ def movs_csv_a_lf(nom_tabla: NomTablas = NomTablas.MOVS,
                                  (pl.col("fecha_agregada").str.to_datetime())
                                  ))
 
-def prods_sql_a_lf(nom_tabla: NomTablas = NomTablas.PRODS,
-                   uri: str = URI) -> pl.LazyFrame:
-    return (cargar_sql_a_df(nom_tabla,uri)
-                                  .with_columns(
+def prods_sql_a_lf(nom_tabla: NomTablas = NomTablas.PRODS) -> pl.LazyFrame:
+    return (cargar_ddb_a_lf(nom_tabla)
+            .with_columns(
         (pl.col("fecha_actualizacion").str.to_datetime(time_unit="ms")))
-                                  .cast({"simulado": pl.Boolean,
+            .cast({"simulado": pl.Boolean,
                                          "riesgo": pl.UInt8,
                                          "liquidez": pl.UInt8,
                                          "plazo": pl.UInt8,
                                          "abierto": pl.Boolean})
-                                  )
+            )
 
-def movs_sql_a_lf(nom_tabla: NomTablas = NomTablas.MOVS,
-                  uri: str = URI) -> pl.LazyFrame:
-    return (cargar_sql_a_df(nom_tabla, uri)
-                   .with_columns((pl.col("fecha").str.to_datetime()),
+def movs_sql_a_lf(nom_tabla: NomTablas = NomTablas.MOVS) -> pl.LazyFrame:
+    return (cargar_ddb_a_lf(nom_tabla)
+            .with_columns((pl.col("fecha").str.to_datetime()),
                                  (pl.col("fecha_agregada").str.to_datetime())
                                  )
             )
