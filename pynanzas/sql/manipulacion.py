c6705f0e3c845da0c1518d23ce3f3074c18a784a
Felipe Bonilla <fa.bonilla@Mac-mini-de-PS-2.local>
2025-09-09 17:48:08 -0500

    feat(manipulación): se agregó función copiar de SQL a DuckDB

--- pynanzas/sql/manipulacion.py
+++ pynanzas/sql/manipulacion.py
@@ -1,21 +1,24 @@
 from dataclasses import asdict
+from pathlib import Path
 import sqlite3
 from typing import Any, Optional
 
-from pynanzas.constants import PROD_ID
+import duckdb
+
+from pynanzas.constants import DIR_DATA, PROD_ID
 from pynanzas.sql.definicion import (
-    crear_tabla_sqlite_movs,
-    crear_tabla_sqlite_prods,
+    _crear_tabla_sqlite_movs,
+    _crear_tabla_sqlite_prods,
 )
-from pynanzas.sql.diccionario import PATH_SQLITE, NomTablas, PathDB
+from pynanzas.sql.diccionario import PATH_SQLITE, NombreBD, NomTablas, PathDB
 from pynanzas.sql.esquemas import EsquemaMovs, EsquemaProds
 from pynanzas.sql.sqlite import tabla_existe
 
 
-def insertar_prod(producto: EsquemaProds,
-                  nom_tabla_prods: NomTablas = NomTablas.PRODS,
-                  path_db: PathDB = PATH_SQLITE
-                  ) -> None:
+def _sql_insertar_prod(producto: EsquemaProds,
+                       nom_tabla_prods: NomTablas = NomTablas.PRODS,
+                       path_db: PathDB = PATH_SQLITE
+                       ) -> None:
     from pynanzas.sql.sqlite import tabla_existe
     prod = asdict(producto)
     prod.pop('fecha_actualizacion', None)
@@ -26,8 +29,8 @@ def insertar_prod(producto: EsquemaProds,
         with sqlite3.connect(path_db) as conn:
             cursor: sqlite3.Cursor = conn.cursor()
             if not tabla_existe(cursor, nom_tabla_prods):
-                crear_tabla_sqlite_prods(nom_tabla_prods=nom_tabla_prods,
-                                         path_db=path_db)
+                _crear_tabla_sqlite_prods(nom_tabla_prods=nom_tabla_prods,
+                                          path_db=path_db)
             query: str = (f"INSERT INTO {nom_tabla_prods} ({columnas}) VALUES "
                           f"({placeholders})")
             cursor.execute(query, valores)
@@ -37,7 +40,7 @@ def insertar_prod(producto: EsquemaProds,
         print(f"sql.insertar_prod: error sql {e}")
 
 
-def insertar_mov(
+def _sql_insertar_mov(
         movimiento: EsquemaMovs,
         nom_tabla_movs: NomTablas = NomTablas.MOVS,
         path_db: PathDB = PATH_SQLITE,
@@ -56,7 +59,7 @@ def insertar_mov(
     with sqlite3.connect(path_db) as conn:
         cursor: sqlite3.Cursor = conn.cursor()
         if not tabla_existe(cursor, nom_tabla_movs):
-            crear_tabla_sqlite_movs(nom_tabla_movs=nom_tabla_movs, path_db=path_db)
+            _crear_tabla_sqlite_movs(nom_tabla_movs=nom_tabla_movs, path_db=path_db)
         query: str = (f"INSERT INTO {nom_tabla_movs} "
                       f"({columnas}) "
                       f"VALUES ({placeholders})")
@@ -64,15 +67,15 @@ def insertar_mov(
         conn.commit()  # Agregado: faltaba commit
 
 
-def actualizar_tabla(nom_tabla: NomTablas,
-                     esquema: EsquemaProds | EsquemaMovs,
-                     nom_bd: PathDB = PATH_SQLITE,
-                     cursor: Optional[sqlite3.Cursor] = None
-                     ) -> None:
+def _sql_actualizar_tabla(nom_tabla: NomTablas,
+                          esquema: EsquemaProds | EsquemaMovs,
+                          nom_bd: PathDB = PATH_SQLITE,
+                          cursor: Optional[sqlite3.Cursor] = None
+                          ) -> None:
 
     from pynanzas.sql.definicion import (
-        crear_tabla_sqlite_movs,
-        crear_tabla_sqlite_prods,
+        _crear_tabla_sqlite_movs,
+        _crear_tabla_sqlite_prods,
     )
 
     with (sqlite3.connect(nom_bd) as con):
@@ -82,12 +85,12 @@ def actualizar_tabla(nom_tabla: NomTablas,
                        "type='table' AND name=?", (nom_tabla,))
         if not cursor.fetchall():
             if isinstance(esquema, EsquemaProds):
-                crear_tabla_sqlite_prods(esquema, nom_tabla, nom_bd)
+                _crear_tabla_sqlite_prods(esquema, nom_tabla, nom_bd)
             elif isinstance(esquema, EsquemaMovs):
                 if not tabla_existe(cursor, NomTablas.PRODS):
-                    crear_tabla_sqlite_prods(path_db=nom_bd)
-                crear_tabla_sqlite_movs(esquema, nom_tabla, NomTablas.PRODS,
-                                        PROD_ID, nom_bd)
+                    _crear_tabla_sqlite_prods(path_db=nom_bd)
+                _crear_tabla_sqlite_movs(esquema, nom_tabla, NomTablas.PRODS,
+                                         PROD_ID, nom_bd)
             return
         cursor.execute(f"PRAGMA table_info ({nom_tabla})")
         resultado: list[Any] = cursor.fetchall()
@@ -110,74 +113,20 @@ def actualizar_tabla(nom_tabla: NomTablas,
                 con.commit()
             return
 
+def _copy_sqlite_ddb(nom_sqllite: NombreBD = NombreBD.SQLITE,
+                     nom_ddb: NombreBD = NombreBD.DDB,
+                     dir_data: Path = DIR_DATA)->None:
 
-# def _reset_sql_desde_csv(dir_data: Path = DIR_DATA,
-#                          path_db: PathDB = PATH_SQLITE,
-#                          nom_tabla_prods: NomTablas = NomTablas.PRODS,
-#                          nom_tabla_movs: NomTablas = NomTablas.MOVS,
-#                          ) -> list[Any]:
-#
-#     from pynanzas.limpiar_data import prods_csv_a_df
-#     df_prods: pd.DataFrame = prods_csv_a_df(nom_tabla_prods)
-#     from pynanzas.limpiar_data import movs_csv_a_df
-#     df_movs: pd.DataFrame = movs_csv_a_df(nom_tabla_movs)
-#
-#     if os.path.exists(path_db): #TODO: Pasar a Path
-#         os.rename(path_db,path_db + '_old')
-#
-#     from pynanzas.sql.definicion import (
-#         crear_tabla_sqlite_movs,
-#         crear_tabla_sqlite_prods,
-#     )
-#
-#     crear_tabla_sqlite_prods(path_db = path_db)
-#     crear_tabla_sqlite_movs(path_db = path_db)
-#
-#     with (sqlite3.connect(path_db) as con):
-#         cursor = con.cursor()
-#         from pynanzas.sql import insertar_prod
-#         for i in df_prods.index:
-#             print(i)
-#             prod = EsquemaProds(
-#                 i,
-#                 df_prods.loc[i]["nombre"],
-#                 df_prods.loc[i]["ticket"],
-#                 bool(df_prods.loc[i]["simulado"]),
-#                 str(df_prods.loc[i]["moneda"]).lower(),
-#                 Riesgo(df_prods.loc[i]["riesgo"]),
-#                 Liquidez(df_prods.loc[i]["liquidez"]),
-#                 Plazo(df_prods.loc[i]["plazo"]),
-#                 df_prods.loc[i]["objetivo"],
-#                 df_prods.loc[i]["administrador"],
-#                 df_prods.loc[i]["plataforma"],
-#                 df_prods.loc[i]["tipo_producto"],
-#                 df_prods.loc[i]["tipo_inversion"],
-#             )
-#             insertar_prod(prod,nom_tabla_prods,path_db)
-#
-#         from pynanzas.sql import insertar_mov
-#         for i in df_movs.index:
-#             mov = EsquemaMovs(
-#                 df_movs.loc[i]["producto_id"],
-#                 df_movs.loc[i]["fecha"].to_pydatetime(),
-#                 df_movs.loc[i]["tipo"],
-#                 df_movs.loc[i]["valor"],
-#                 df_movs.loc[i]["unidades"],
-#                 df_movs.loc[i]["valor_unidades"],
-#             )
-#             insertar_mov(mov, nom_tabla_movs, path_db)
-#         cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-#         retorno: list[Any]= cursor.fetchall()
-#         if retorno:
-#             from pynanzas.export import exportar_sql_csv
-#
-#             os.rename(dir_data / (nom_tabla_prods+'.csv'),
-#                       dir_data / ('_old_'+nom_tabla_prods+'.csv'))
-#             exportar_sql_csv(nom_tabla_prods)
-#
-#             os.rename(dir_data / (nom_tabla_movs + ".csv"),
-#                       dir_data / ("_old_" + nom_tabla_movs + ".csv"))
-#             exportar_sql_csv(nom_tabla_movs)
-#             return retorno
-#         else:
-#             raise
+    with duckdb.connect() as con:
+        try:
+            q = (f"""ATTACH DATABASE '{dir_data / nom_sqllite}' AS sqlite;\n"""
+                        f"""ATTACH DATABASE '{dir_data / nom_ddb}' AS ddb;\n"""
+                        f"""COPY FROM DATABASE sqlite to ddb;\n"""
+                        f"""DROP TABLE ddb.sqlite_sequence;""")
+            print(q)
+            con.execute(q)
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    _copy_sqlite_ddb()
