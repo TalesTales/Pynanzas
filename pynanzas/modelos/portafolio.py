b6a2e2b09e195e7b045c535bb7623815b44685f9
TalesTales <servicios@pipebonilla.com>
2025-09-30 09:20:53 -0500

    feat(portafolio): Se agregó el método saldo_hist a la clase
    Devuelve un pl.df de [n, n]. Con una serie temporal (primera columna 'fecha') y todos los prods como columnas

--- pynanzas/modelos/portafolio.py
+++ pynanzas/modelos/portafolio.py
@@ -192,137 +192,25 @@ class Portafolio:
             if p.producto_id in prods
         )
 
-    #     # Extraer la xirr desde Producto Financierto
-    #     # Apendizarla en un df que sea fecha y add column producto id
-    #     # ordenar por fecha
-    #     # Fill null con anterior
+    def saldo_hist(self,
+                   *,
+                   abierto: bool | None  = True,
+                   riesgo: list[Riesgo] | None = None,
+                   plazo: list[Plazo] | None = None,
+                   liquidez: list[Liquidez] | None = None,
+                   simulado: bool | None = False ) -> pl.DataFrame:
 
-    # def balancear(self, monto_invertir: float) -> dict[str, float]:
-    #     portafolio_actual: float = self.total
-    #     portafolio_futuro: float = portafolio_actual + monto_invertir
-    #     portafolio = self.prods
-    #     # Calcular valores objetivos para cada producto
-    #     valores_actuales: dict[str, float] = {}
-    #     valores_objetivo: dict[str, float] = {}
-    #     diferencias: dict[str, float] = {}
-    #
-    #     for ticker, producto in portafolio.items():
-    #         if producto.asignacion >= 0:
-    #             valor_actual = producto.saldo
-    #             valor_objetivo = portafolio_futuro * producto.asignacion
-    #             diferencia = valor_objetivo - valor_actual
-    #
-    #             valores_actuales[ticker] = valor_actual
-    #             valores_objetivo[ticker] = valor_objetivo
-    #             diferencias[ticker] = diferencia
-    #
-    #             # print(f"\n{ticker}:")
-    #             # print(f"  Peso actual: {producto.peso:.2%}")
-    #             # print(f"  Peso objetivo: {producto.asignacion:.2%}")
-    #             # print(f"  Valor actual: ${valor_actual:,.2f}")
-    #             # print(f"  Valor objetivo: ${valor_objetivo:,.2f}")
-    #             # print(f"  Diferencia: ${diferencia:,.2f}")
-    #
-    #     diferencias_positivas: dict[str, float] = {
-    #         k: v for k, v in diferencias.items() if v > 0
-    #     }
-    #
-    #     suma_diferencias_positivas: float = sum(diferencias_positivas.values())
-    #
-    #     distribucion: dict[str, float] = {}
-    #
-    #     if suma_diferencias_positivas > 0:
-    #         for ticker, diferencia in diferencias_positivas.items():
-    #             proporcion = diferencia / suma_diferencias_positivas
-    #             monto_asignado = np.trunc(monto_invertir * proporcion)
-    #             distribucion[ticker] = monto_asignado
-    #     else:
-    #         for ticker, producto in portafolio.items():
-    #             if producto.asignacion >= 0 and not np.isnan(producto.saldo):
-    #                 distribucion[ticker] = monto_invertir * producto.asignacion
-    #
-    #     # print("DISTRIBUCIÓN SUGERIDA DE LA NUEVA INVERSIÓN:")
-    #
-    #     total_distribuido: float = 0
-    #     for ticker, monto in distribucion.items():
-    #         if monto > 0:
-    #             porcentaje = (monto / monto_invertir) * 100
-    #             # print(f"{ticker}: ${monto:,.2f} ({porcentaje:.1f}%)")
-    #             total_distribuido += monto
-    #
-    #     # print(f"\nTotal distribuido: ${total_distribuido:,.2f}")
-    #     #
-    #     # print("PESOS RESULTANTES DESPUÉS DE LA INVERSIÓN:")
-    #
-    #     for ticker, producto in portafolio.items():
-    #         if producto.asignacion >= 0 and not np.isnan(producto.saldo):
-    #             valor_final = valores_actuales[ticker] + distribucion.get(
-    #                 ticker, 0
-    #             )
-    #             peso_final = valor_final / portafolio_futuro
-    #             diferencia_objetivo = peso_final - producto.asignacion
-    #
-    #             # print(f"{ticker}:")
-    #             # print(f"  Peso final: {peso_final:.2%}")
-    #             # print(f"  Objetivo: {producto.asignacion:.2%}")
-    #             # print(f"  Diferencia: {diferencia_objetivo:+.2%}")
-    #     return distribucion
-    # def saldos_hist(self) -> pl.DataFrame:
-    #     portafolio = self.prods
-    #     historico_acumulado_df: pd.DataFrame = pd.DataFrame()
-    #
-    #     for i, (ticker, producto) in enumerate(portafolio.items()):
-    #         if abiertos is not None and abiertos != producto.abierto:
-    #             continue
-    #
-    #         if simulados is not None and simulados != producto.simulado:
-    #             continue
-    #
-    #         if (
-    #             producto._movs_hist.empty
-    #             or "saldo_historico" not in producto._movs_hist.columns
-    #         ):
-    #             continue
-    #
-    #         producto_saldo = producto._movs_hist[
-    #             ["fecha", "saldo_historico"]
-    #         ].copy()
-    #         producto_saldo = producto_saldo[
-    #             ~producto_saldo["saldo_historico"].isna()
-    #         ]
-    #
-    #         if producto_saldo.empty:
-    #             continue
-    #         producto_saldo = producto_saldo.groupby("fecha").last()
-    #         producto_saldo = producto_saldo.rename(
-    #             columns={"saldo_historico": ticker}
-    #         )
-    #
-    #         if historico_acumulado_df.empty:
-    #             historico_acumulado_df = producto_saldo
-    #         else:
-    #             historico_acumulado_df = pd.concat(
-    #                 [historico_acumulado_df, producto_saldo], axis=1
-    #             )
-    #
-    #     if not historico_acumulado_df.empty:
-    #         historico_acumulado_df = historico_acumulado_df.sort_index()
-    #         historico_acumulado_df = historico_acumulado_df.ffill()
-    #         # último valor
-    #     return historico_acumulado_df
-    #
-    # def saldos_porcent_hist(
-    #     self, abiertos: bool = True, simulados: bool = False
-    # ) -> pd.DataFrame:
-    #     saldos: pd.DataFrame = self.saldos_hist(abiertos, simulados)
-    #     totales: Series[float] = self.saldos_hist(abiertos, simulados).sum(
-    #         axis=1
-    #     )
-    #     df_saldos_porcent_hist: pd.DataFrame = saldos.div(totales, axis=0)
-    #     return df_saldos_porcent_hist
-    #
-    # def dist_riesgo(self) -> pd.Series:  # TODO: Se puede optimizar
-    #     return dist_riesgo(productos=self.prods)
+        prods = set(self._filtro(abierto, riesgo, plazo, liquidez,
+                              simulado)
+                    .select(pl.col(PROD_ID))
+                    .collect().to_series().to_list())
+
+        lista_saldo = [p.saldo_hist.rename({'saldo_hist':p.producto_id})
+                      for p in self.prods.values()
+                      if p.producto_id in prods]
+
+        saldo_hist = pl.concat(lista_saldo, how='align')
+        return saldo_hist
 
 if __name__ == '__main__':
     port = Portafolio()
